147457a2df8c1f714be8c352e211f9434bfc0d01
Niel Bornstein <niel@bornstein.atlanta.ga.us>
2017-12-17 09:48:24 -0500

    Added mute and report as actions

--- KillPopStars.py
+++ KillPopStars.py
@@ -6,6 +6,11 @@ import sys
 # parameter 1 is the ID of the tweet to start with
 tweet_id = sys.argv[1]
 
+# paramter 2 is the action to take - default is to block, but you can also mute or report.
+action = 'block'
+if sys.argv.count > 2:
+    action = sys.argv[2]
+
 # read the Twitter API auth from 'config' file
 auth = {}
 f = open('config')
@@ -19,15 +24,33 @@ t = Twitter(auth=OAuth(auth['consumer_key'], auth['consumer_secret'], auth['acce
 # get the base tweet
 tweet = t.statuses.show(id=tweet_id)
 
-# block the user
-print 'Blocking original tweeter @'  + tweet['user']['screen_name'] + ' (' + tweet['user']['name'] + ')!'
-t.blocks.create(screen_name=tweet['user']['screen_name'])
+# take action on the user
+if (action == 'block'):
+    print 'Blocking original tweeter @'  + tweet['user']['screen_name'] + ' (' + tweet['user']['name'] + ')!'
+    t.blocks.create(screen_name=tweet['user']['screen_name'])
+elif (action == 'mute'):
+    print 'Muting original tweeter @'  + tweet['user']['screen_name'] + ' (' + tweet['user']['name'] + ')!'
+    t.mutes.users.create(screen_name=tweet['user']['screen_name'])
+elif (action == 'report'):
+    print 'Reporting original tweeter @'  + tweet['user']['screen_name'] + ' (' + tweet['user']['name'] + ')!'
+    t.users.report_spam(screen_name=tweet['user']['screen_name'])
+else:
+    print 'No action for original tweeter @'  + tweet['user']['screen_name'] + ' (' + tweet['user']['name'] + ')!'
 
 # get all retweets
 retweets = t.statuses.retweets(id=tweet_id)
 
-# block everyone who retweeted it
+# take action on everyone who retweeted it
 for retweet in retweets:
-    print 'Blocking retweeter @'  + retweet['user']['screen_name'] + ' (' + retweet['user']['name'] + ')!'
-    t.blocks.create(screen_name=retweet['user']['screen_name'])
+    if (action == 'block'):
+        print 'Blocking retweeter @'  + retweet['user']['screen_name'] + ' (' + retweet['user']['name'] + ')!'
+        t.blocks.create(screen_name=retweet['user']['screen_name'])
+    elif (action == 'mute'):
+        print 'Muting retweeter @'  + retweet['user']['screen_name'] + ' (' + retweet['user']['name'] + ')!'
+        t.mutes.users.create(screen_name=retweet['user']['screen_name'])
+    elif (action == 'report'):
+        print 'Reporting retweeter @'  + retweet['user']['screen_name'] + ' (' + retweet['user']['name'] + ')!'
+        t.users.report_spam(screen_name=retweet['user']['screen_name'])
+    else:
+        print 'No action for retweeter @'  + retweet['user']['screen_name'] + ' (' + retweet['user']['name'] + ')!'
 
